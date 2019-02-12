f31f0915c8edc2c052b157c1c194ccc73ae02091
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-02-12 10:21:23 +0530

    rewrote convolute.py and removed convolution.py

--- convolute.py
+++ convolute.py
@@ -1,33 +1,37 @@
 import cv2
+from kernel import Kernel
 import numpy as np
-from copy import deepcopy
 
-image = cv2.imread("image.jpg", cv2.IMREAD_GRAYSCALE)
-#negative
-image_negative = deepcopy(image)
-for x in range(len(image)):
-    for y in range(len(image[0])):
-        image_negative[x][y] = 255 -  image[x][y]
-cv2.imwrite("image_neg.jpg",image_negative)
-#cv2.waitKey(10000)
-kernel_size = 7
-padded_image = np.zeros((kernel_size - 1 + len(image), kernel_size - 1 + len(image[0])))
-for x in range(len(image)):
-    for y in range(len(image[0])):
-        padded_image[int(kernel_size/2) + x][int(kernel_size/2) + y] = image_negative[x][y]
+class Convolute:
+    def __init__(self, image_name, kernel_type):
+        self.image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+        self.kernel = Kernel(kernel_type)
+        self.kernel = self.kernel.getKernel()
+        self.kernel_size = 3
+        self.type = kernel_type
 
-kernel = np.full((kernel_size, kernel_size), 1)
+    def convolution(self):
+        padded_image = np.zeros((self.kernel_size - 1 + len(self.image), self.kernel_size - 1 + len(self.image[0])))
+        for x in range(len(self.image)):
+            for y in range(len(self.image[0])):
+                padded_image[int(self.kernel_size/2) + x][int(self.kernel_size/2) + y] = self.image[x][y]
 
-mod_image = np.zeros((len(image), len(image[0])))
+        mod_image = np.zeros((len(self.image), len(self.image[0])))
+        for i in range(len(self.image)):
+            for j in range(len(self.image[0])):
+                val = 0
+                for x in range(-1*int((self.kernel_size-1)/2), int((self.kernel_size-1)/2) + 1):
+                    for y in range(-int((self.kernel_size-1)/2), int((self.kernel_size-1)/2) + 1):
+                        val += padded_image[x + i][y + j]*self.kernel[x][y]
+                mod_image[i][j] = val
+        cv2.imwrite("{}.jpg".format(self.type),mod_image)
 
+if __name__ == "__main__":
+    print("Choose from the following:\n IDENTITY \t EDGE_DETECTION1/2/3 \t BOX \t GAUSS_BLUR\n SHARPEN")
+    response = input()
+    if response not in ["IDENTITY", "EDGE_DETECTION1", "EDGE_DETECTION2", "EDGE_DETECTION3", "BOX", "GAUSS_BLUR", "SHARPEN"]:
+        print("Enter the right response next time :)")
+        exit()
 
-
-for i in range(len(image)):
-    for j in range(len(image[0])):
-        val = 0
-        for x in range(-1*int((kernel_size-1)/2), int((kernel_size-1)/2) + 1):
-            for y in range(-int((kernel_size-1)/2), int((kernel_size-1)/2) + 1):
-                val += padded_image[x + i][y + j]*kernel[x][y]
-        val /= kernel_size**2
-        mod_image[i][j] = val
-cv2.imwrite("image_conv.jpg",mod_image)
+    yo = Convolute("image.jpg", response)
+    yo.convolution()
